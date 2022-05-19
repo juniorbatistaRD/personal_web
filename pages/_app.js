7a6be2099899bb9f7222ba5a27d26031387148cc
Junior Batista <31187922+juniorbatistaRD@users.noreply.github.com>
2022-05-19 10:06:33 -0400

    Google analytics measure only on production

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,14 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <NextIntlProvider messages={pageProps.messages}>
-      {/* {process.env.NODE_ENV === "production" && (
-          <> */}
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <Script id="google-analytics" strategy="lazyOnload">
+            {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -21,9 +21,9 @@ function MyApp({ Component, pageProps }) {
               page_path: window.location.pathname,
             });
                 `}
-      </Script>
-      {/* </>
-        )} */}
+          </Script>
+        </>
+      )}
       <Component {...pageProps} />
     </NextIntlProvider>
   );
